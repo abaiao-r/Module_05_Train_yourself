f947d97d629607c46f5b6cf6e9912a7f68f6d531
André Francisco <andrefranciscorolao@gmail.com>
2024-09-19 04:10:27 +0100

    create clang format file

--- ex00/includes/City.hpp
+++ ex00/includes/City.hpp
@@ -3,28 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   City.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: andrefrancisco <andrefrancisco@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 13:48:35 by abaiao-r          #+#    #+#             */
-/*   Updated: 2024/09/16 13:48:59 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2024/09/19 03:49:51 by andrefranci      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CITY_HPP
-# define CITY_HPP
+#define CITY_HPP
 
-# include "libraries.hpp"
+#include "libraries.hpp"
 
 class City : public Node
 {
-    private:
+	private:
 
-    public:
-        City();
-        City(const City &src);
-        ~City();
-        City &operator=(const City &src);
-
+	public:
+		City();
+		City(const City &src);
+		~City();
+		City &operator=(const City &src);
 };
 
 #endif
