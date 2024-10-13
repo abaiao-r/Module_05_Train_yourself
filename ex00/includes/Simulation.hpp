cdc69bc4cefecb8ef3aef7c26997b317fdcd54d0
abaiao-r <andrefranciscorolao@gmail.com>
2024-10-13 23:16:56 +0100

    feat: Add Node class with custom exceptions and test main function
    - Implemented Node class with the following features:
      - Constructor to initialize node with a unique name.
      - Destructor to clean up resources.
      - Method to add edges between nodes with distance.
      - Methods to get node name and edges.
      - Custom exceptions for self-edge, duplicate edge, and duplicate node name.
    
    - Added a main function to test the Node class:
      - Created nodes and added edges.
      - Demonstrated handling of custom exceptions.
      - Printed node names and their edges.
    
    - Updated Makefile to include new source files and set C++14 standard.

--- ex00/includes/Simulation.hpp
+++ ex00/includes/Simulation.hpp
@@ -3,28 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   Simulation.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
+/*   By: andrefrancisco <andrefrancisco@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 14:06:52 by abaiao-r          #+#    #+#             */
-/*   Updated: 2024/09/16 14:07:05 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2024/10/13 19:57:24 by andrefranci      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIMULATION_HPP
-# define SIMULATION_HPP
+#define SIMULATION_HPP
 
-# include "libraries.hpp"
+#include "libraries.hpp"
 
 class Simulation
 {
-    private:
+	private:
 
-    public:
-        Simulation();
-        Simulation(const Simulation &src);
-        ~Simulation();
-        Simulation &operator=(const Simulation &src);
-
+	public:
+		Simulation();
+		Simulation(const Simulation &src);
+		~Simulation();
+		Simulation &operator=(const Simulation &src);
 };
 
 #endif
---
+++ ex00/src/Node.cpp
@@ -0,0 +1,137 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Node.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andrefrancisco <andrefrancisco@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 21:11:20 by andrefranci       #+#    #+#             */
+/*   Updated: 2024/10/13 23:12:54 by andrefranci      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Node.hpp"
+
+std::unordered_set<std::string> Node::_existingNames;
+
+// Parameterized constructor
+Node::Node(std::string name) : _name(name)
+{
+	if (_existingNames.find(name) != _existingNames.end())
+	{
+		throw DuplicateNodeException(name);
+	}
+	_existingNames.insert(name);
+}
+
+// Destructor
+Node::~Node()
+{
+}
+
+// Add an edge to the node
+void Node::addEdge(std::weak_ptr<Node> node, size_t distance)
+{
+	// Check if the node is trying to add itself
+	std::shared_ptr<Node> sharedNode = node.lock();
+	if (sharedNode)
+	{
+		if (sharedNode.get() == this)
+		{
+			throw SelfEdgeException(this->_name);
+		}
+	}
+
+	// Check if the edge already exists
+	for (const std::pair<std::weak_ptr<Node>, size_t> &edge : _edges)
+	{
+		std::shared_ptr<Node> existingNode = edge.first.lock();
+		if (existingNode)
+		{
+			if (existingNode == sharedNode)
+			{
+				throw EdgeAlreadyExistsException(this->_name,
+												 sharedNode->getName());
+			}
+		}
+	}
+	_edges.push_back(std::make_pair(node, distance));
+}
+
+// Get the name of the node
+const std::string &Node::getName() const
+{
+	return (this->_name);
+}
+
+// Get the edges of the node
+const std::vector<std::pair<std::weak_ptr<Node>, size_t>> &Node::getEdges()
+	const
+{
+	return (this->_edges);
+}
+
+// main to test the Node class
+
+/* int main()
+{
+	try
+	{
+		// Create nodes
+		std::shared_ptr<Node> nodeA = std::make_shared<Node>("A");
+		std::shared_ptr<Node> nodeB = std::make_shared<Node>("B");
+		std::shared_ptr<Node> nodeC = std::make_shared<Node>("C");
+
+		// Add edges
+		nodeA->addEdge(nodeB, 10);
+		nodeA->addEdge(nodeC, 20);
+
+		// Attempt to add a duplicate edge
+		try
+		{
+			nodeA->addEdge(nodeB, 10);
+		}
+		catch (const Node::EdgeAlreadyExistsException &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+
+		// Attempt to add a self-edge
+		try
+		{
+			nodeA->addEdge(nodeA, 5);
+		}
+		catch (const Node::SelfEdgeException &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+
+		// Attempt to create a node with a duplicate name
+		try
+		{
+			std::shared_ptr<Node> nodeDuplicate = std::make_shared<Node>("A");
+		}
+		catch (const Node::DuplicateNodeException &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+
+		// Print node names and edges
+		std::cout << "Node " << nodeA->getName()
+				  << " has edges to:" << std::endl;
+		for (const auto &edge : nodeA->getEdges())
+		{
+			if (auto targetNode = edge.first.lock())
+			{
+				std::cout << "  Node " << targetNode->getName()
+						  << " with distance " << edge.second << std::endl;
+			}
+		}
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+
+	return 0;
+} */
