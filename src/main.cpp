89815b28e131c8f781fc3146e7406deb51570ad2
Andre Francisco <andrefranciscorolao@gmail.com>
2024-10-24 19:58:57 +0100

    Update CI workflow and Makefile to include steps for setting up Node.js, C++ environment, and running tests

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: andrefrancisco <andrefrancisco@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 16:58:42 by andrefranci       #+#    #+#             */
-/*   Updated: 2024/09/19 04:04:32 by andrefranci      ###   ########.fr       */
+/*   Updated: 2024/10/24 19:58:43 by andrefranci      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	}
 
-	try
+	/* try
 	{
 		// Create an InputHandler object with the provided file paths
 		InputHandler inputHandler(argv[1], argv[2]);
@@ -40,7 +40,7 @@ int main(int argc, char **argv)
 	{
 		std::cerr << "Error: " << ex.what() << std::endl;
 		return EXIT_FAILURE;
-	}
+	} */
 
 	return (EXIT_SUCCESS);
 }
